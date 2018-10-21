74596b683d216c1687c95331dcca738ef94ab727
TatianaVolkova <44113785+TatianaVolkova@users.noreply.github.com>
2018-10-21 14:17:12 +0200

    logistic funtion implementation

---
+++ helpers.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+"""some helper functions."""
+import numpy as np
+
+def standardize(x, mean_x=None, std_x=None):
+    """Standardize the original data set."""
+    if mean_x is None:
+        mean_x = np.mean(x, axis=0)
+    x = x - mean_x
+    if std_x is None:
+        std_x = np.std(x, axis=0)
+    x[:, std_x>0] = x[:, std_x>0] / std_x[std_x>0]
+    
+    tx = np.hstack((np.ones((x.shape[0],1)), x))
+    return tx, mean_x, std_x
+
+
+def batch_iter(y, tx, batch_size, num_batches=None, shuffle=True):
+   
+    data_size = len(y)
+    num_batches_max = int(np.ceil(data_size/batch_size))
+    if num_batches is None:
+        num_batches = num_batches_max
+    else:
+        num_batches = min(num_batches, num_batches_max)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
