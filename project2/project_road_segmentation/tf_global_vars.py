428b48552a93fa9c01ea9f85b7bd9c5a1993196e
ZiluTian <zilu.tian@epfl.ch>
2018-12-13 22:14:41 +0100

    Add the missing files

---
+++ project2/project_road_segmentation/tf_global_vars.py
@@ -0,0 +1,23 @@
+NUM_CHANNELS = 3 # RGB images
+PIXEL_DEPTH = 255
+NUM_LABELS = 2
+TRAINING_SIZE = 100
+TESTING_SIZE = 50
+TRAIN_PER = .7  # Size of the training set.
+SEED = 66478  # Set to None for random seed.
+NP_SEED= 78456
+BATCH_SIZE = 16 # 64
+NUM_EPOCHS = 5
+RESTORE_MODEL = True # If True, restore existing model instead of training a new one
+RECORDING_STEP = 0
+FILTER_SIZE = 3
+# Set image patch size in pixels
+# IMG_PATCH_SIZE should be a multiple of 4
+# image size should be an integer multiple of this number!
+IMG_PATCH_SIZE = 16 
+BORDER = 8
+IMG_TOTAL_SIZE = IMG_PATCH_SIZE + 2 * BORDER
+
+CONV_ARCH = [1, 1]
+OUTPUT_CHANNELS = [32, 64]
+
