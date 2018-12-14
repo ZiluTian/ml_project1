54b1a87e5ed283f31c45b35d16785df08647cbb8
Virginia Bordignon <virginia.bordignon@epfl.ch>
2018-12-14 17:00:18 +0100

    My code working good

---
+++ project2/project_road_segmentation/Virginia_code/tf_utils.py
@@ -0,0 +1,44 @@
+# General helper functions
+import numpy
+import tensorflow as tf
+from tf_global_vars import *
+from tf_img_helpers import *
+
+def error_rate(predictions, labels):
+    """Return the error rate based on dense predictions and 1-hot labels."""
+    return 100.0 - (
+        100.0 *
+    numpy.sum(numpy.argmax(predictions, 1) == numpy.argmax(labels, 1)) /
+        predictions.shape[0])
+
+
+def f1_score(predictions, labels):
+    pred_pos = numpy.argmax(predictions, 1) == 1
+    pred_neg = numpy.argmax(predictions, 1) == 0
+
+    n_pos = numpy.argmax(labels, 1) == 1
+    n_neg = numpy.argmax(labels, 1) == 0
+
+    TP = numpy.sum(numpy.logical_and(pred_pos, n_pos))
+    FP = numpy.sum(numpy.logical_and(pred_pos, n_neg))
+    FN = numpy.sum(numpy.logical_and(pred_neg, n_pos))
+
+    return (TP, FP, FN)
+
+# Write predictions from neural network to a file
+# def write_predictions_to_file(predictions, labels, filename):
+#     max_labels = numpy.argmax(labels, 1)
+#     max_predictions = numpy.argmax(predictions, 1)
+#     file = open(filename, "w")
+#     n = predictions.shape[0]
+#     for i in range(0, n):
+#         file.write(max_labels(i) + ' ' + max_predictions(i))
+#     file.close()
+
+# Print predictions from neural network
+# def print_predictions(predictions, labels):
+#     max_labels = numpy.argmax(labels, 1)
+#     max_predictions = numpy.argmax(predictions, 1)
+#     print (str(max_labels) + ' ' + str(max_predictions))
+
+    # Make an image summary for 4d tensor image with index idx
